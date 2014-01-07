6e6f3d08de7656df3bfd37bb99a2303c8aeddff4
crislanerabelo <crislane_rabelo@hotmail.com>
2014-01-07 18:32:54 -0200

    Create form_remover.php

---
+++ cadastro/form_remover.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>cadastro de pessoas</title>
+    </head>
+    <body>
+        <?php
+            require_once("menu.php");
+        ?>
+        
+        <form action="remover.php" method="post">
+            <label>Digite o codigo da pessoa para remover:</label>
+            <br/>
+            <input type="text" name="id" />
+            <input type="submit" value="Remover" />
+        </form>
+    </body>
+</html>
+
+<?php
+require_once("menu.php");
+
+    session_start();
+    
+    
+    if(isset($_SESSION["cadastros"])){
+        
+        $cadastros = $_SESSION["cadastros"];
+        
+        foreach($cadastros as $id =>$pessoa){
+            if($pessoa != null) {
+                echo "[$id]=>$pessoa <br/>";
+            }
+        }
+    }
+    else{
+        echo "Nao existem pessoas cadastradas";
+    }
+    
+    
+?>
