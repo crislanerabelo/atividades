3604555d0cbd1ca410f3f9beb01496390f54dc6c
crislanerabelo <crislane_rabelo@hotmail.com>
2014-01-09 16:49:38 -0200

    Create Campos_pessoa.php

---
+++ cadastro/Campos_pessoa.php
@@ -0,0 +1,8 @@
+<input type="text" name="nome" />
+<br/>
+<input type="radio" name="sexo" value="m" checked="checked"/>
+<label>Masc<label>Nome:</label>
+ulino</label>
+<input type="radio"bel>
+<br/>  name="sexo" value="f" />
+<label>Feminino</la
